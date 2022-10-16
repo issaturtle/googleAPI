e8fc0a42bb7c16841489014473a378537300899b
Hung Nguyen <enegry135@gmail.com>
2022-10-15 21:50:06 -0700

    fixed

--- makeToken.py
+++ makeToken.py
@@ -1,24 +1,24 @@
-import pickle
+# Hung Nguyen
+# Gmail API implementation for cmpe 188
 import os
+import base64
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from googleapiclient.errors import HttpError
-
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 #code referenced from Google API page
 #https://developers.google.com/gmail/api/quickstart/python
-def getAuthToken(*scopes):
-
-    SCOPES = [scope for scope in scopes[0]]
-   
+def getAuthToken():
+    SCOPES = ['https://mail.google.com/']
     credentials = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
+    #creates token.json file for verification
+    #if file doesn't exist, ask user to verify
     if os.path.exists('token.json'):
         credentials = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
+  
     if not credentials or not credentials.valid:
         if credentials and credentials.expired and credentials.refresh_token:
             credentials.refresh(Request())
@@ -26,14 +26,25 @@ def getAuthToken(*scopes):
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             credentials = flow.run_local_server(port=0)
-        # Save the credentials for the next run
+        
+        #once verified, user can skip verifying process
         with open('token.json', 'w') as token:
             token.write(credentials.to_json())
-
+    
     try:
         # Call the Gmail API
         buildGmailService = build('gmail', 'v1', credentials=credentials)
-        return buildGmailService
+        # return buildGmailService
+        msg = "this is a demo"
+        mimeMsg = MIMEMultipart()
+        mimeMsg['subject'] = "for cmpe"
+        mimeMsg['to'] = "enegry135@gmail.com"
+        mimeMsg.attach(MIMEText(msg,'plain'))
+        #turn message into raw string
+        msgString = base64.urlsafe_b64encode(mimeMsg.as_bytes()).decode()
+        buildGmailService.users().messages().send(userId = 'me', body = {'raw':msgString}).execute()
+
     except HttpError as error:
         print(f'An error occurred: {error}')
 
+getAuthToken()
